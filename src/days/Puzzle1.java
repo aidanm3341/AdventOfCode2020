ba62cc3752f44a9e97225261844cb25b5d349ea2
aidanmcphelim <aidanmcp@hotmail.co.uk>
2020-12-01 14:53:32 +0000

    added project structure and day 1

---
+++ src/days/Puzzle1.java
@@ -0,0 +1,21 @@
+package days;
+
+import utils.InputReader;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Puzzle1 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Stream<String> input = new InputReader().readLines("1.txt");
+        List<Integer> ints = input.map(Integer::parseInt).collect(Collectors.toList());
+
+        for (int i : ints)
+            for (int j : ints)
+                for (int k : ints)
+                    if(i + j + k == 2020)
+                        System.out.println(i * j * k);
+    }
+}
---
+++ src/utils/InputReader.java
@@ -0,0 +1,15 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.stream.Stream;
+
+public class InputReader {
+
+    public Stream<String> readLines(String src) throws FileNotFoundException {
+        FileReader fr = new FileReader("resources/" + src);
+        BufferedReader br = new BufferedReader(fr);
+        return br.lines();
+    }
+}
