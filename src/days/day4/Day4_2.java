7ba2aaab0b6bcf9a98c9a64eb2a78e7a4e11fffd
aidanmcphelim <aidanmcp@hotmail.co.uk>
2020-12-04 14:05:24 +0000

    made the parsing a liiiiiittle nicer

--- src/days/day4/Day4_2.java
+++ src/days/day4/Day4_2.java
@@ -9,29 +9,26 @@ import java.util.stream.Stream;
 
 public class Day4_2 {
 
-    private static PassportCandidate parsePassport(Stream<String> p){
+    private static PassportCandidate parsePassport(String p){
         Set<String> requiredFields = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
 
-        Map<String, String> output = new HashMap<>();
-        p.forEach(s -> {
-            for (String pair : s.split(" "))
-                output.put(pair.substring(0, 3), pair.substring(4));
-        });
+        Map<String, String> fields = new HashMap<>();
+        for (String segment : p.split(" ")) {
+            String[] pairs = segment.split(":");
+            fields.put(pairs[0], pairs[1]);
+        }
 
-        PassportCandidate pc;
-        if(output.keySet().containsAll(requiredFields))
-            pc = new Passport(output.get("byr"),
-                    output.get("iyr"),
-                    output.get("eyr"),
-                    output.get("hgt"),
-                    output.get("hcl"),
-                    output.get("ecl"),
-                    output.get("pid"));
+        if(fields.keySet().containsAll(requiredFields))
+            return new Passport(fields.get("byr"),
+                    fields.get("iyr"),
+                    fields.get("eyr"),
+                    fields.get("hgt"),
+                    fields.get("hcl"),
+                    fields.get("ecl"),
+                    fields.get("pid"));
         else
-            pc = new InvalidPassport();
-        return pc;
+            return new InvalidPassport();
     }
-    
 
     public static void main(String[] args) throws FileNotFoundException {
         Stream<String> inputs = new InputReader().readAsStream("4.txt");
@@ -39,17 +36,20 @@ public class Day4_2 {
         int numberOfValidPassports = 0;
         List<String> lines = new ArrayList<>();
         for(String line : inputs.collect(Collectors.toList())) {
-            if(!line.equals("")){
+            if(!line.equals(""))
                 lines.add(line);
-            }
             else{
-                if(parsePassport(lines.stream()).isValidPassport())
+                String passportLine = lines.stream().map(s -> s + " ").collect(Collectors.joining());
+                if(parsePassport(passportLine).isValidPassport())
                     numberOfValidPassports++;
                 lines.clear();
             }
         }
-        if(parsePassport(lines.stream()).isValidPassport())
+
+        String passportLine = lines.stream().map(s -> s + " ").collect(Collectors.joining());
+        if(parsePassport(passportLine).isValidPassport())
             numberOfValidPassports++;
+
         System.out.println(numberOfValidPassports);
     }
 }
