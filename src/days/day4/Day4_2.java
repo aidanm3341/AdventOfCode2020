f8d8df537afaf6c080d1cf0a3faf23298ec06e9d
aidanmcphelim <aidanmcp@hotmail.co.uk>
2020-12-04 12:01:54 +0000

    removed the old validity checking

--- src/days/day4/Day4_2.java
+++ src/days/day4/Day4_2.java
@@ -31,64 +31,7 @@ public class Day4_2 {
             pc = new InvalidPassport();
         return pc;
     }
-
-    public static boolean isValidPassport(Map<String, String> passport){
-        Set<String> requiredFields = Set.of("byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid");
-
-        Set<String> fields = passport.keySet();
-        return fields.containsAll(requiredFields) &&
-                isBirthYearValid(passport.get("byr")) &&
-                isIssueYearValid(passport.get("iyr")) &&
-                isExpirationYearValid(passport.get("eyr")) &&
-                isHeightValid(passport.get("hgt")) &&
-                isValidHairColour(passport.get("hcl")) &&
-                isValidEyeColour(passport.get("ecl")) &&
-                isValidPassportID(passport.get("pid"));
-    }
-
-    private static boolean isBirthYearValid(String s){
-        if(s.length() != 4) return false;
-        int val = Integer.parseInt(s);
-        return val >= 1920 && val <= 2002;
-    }
-
-    private static boolean isIssueYearValid(String s){
-        if(s.length() != 4) return false;
-        int val = Integer.parseInt(s);
-        return val >= 2010 && val <= 2020;
-    }
-
-    private static boolean isExpirationYearValid(String s){
-        if(s.length() != 4) return false;
-        int val = Integer.parseInt(s);
-        return val >= 2020 && val <= 2030;
-    }
-
-    private static boolean isHeightValid(String s){
-        if(s.endsWith("cm")){
-            int val = Integer.parseInt(s.substring(0, s.length() - 2));
-            return val >= 150 && val <= 193;
-        }
-        else if(s.endsWith("in")){
-            int val = Integer.parseInt(s.substring(0, s.length() - 2));
-            return val >= 59 && val <= 76;
-        }
-        else
-            return false;
-    }
-
-    private static boolean isValidHairColour(String s){
-        return s.matches("#[0-9a-f]{6}");
-    }
-
-    private static boolean isValidEyeColour(String s){
-        Set<String> validColours = Set.of("amb", "blu", "brn", "gry", "grn", "hzl", "oth");
-        return validColours.contains(s);
-    }
-
-    private static boolean isValidPassportID(String s){
-        return s.matches("[0-9]{9}");
-    }
+    
 
     public static void main(String[] args) throws FileNotFoundException {
         Stream<String> inputs = new InputReader().readAsStream("4.txt");
