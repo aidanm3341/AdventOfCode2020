e84aa01ab5d0a9d7f64d2606ba5154ef5ea7d0b9
aidanmcphelim <aidanmcp@hotmail.co.uk>
2020-12-07 08:34:26 +0000

    day6

---
+++ src/days/Day6_1.java
@@ -0,0 +1,43 @@
+package days;
+
+import utils.InputReader;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day6_1 {
+    private static int calcCountOfUniqueAnswersInGroup(String s){
+        Map<Character, Integer> answerCount = new HashMap<>();
+        Stream<Character> chars = s.chars().mapToObj(c -> (char) c);
+        chars.forEach(c -> answerCount.put(c, answerCount.getOrDefault(c, 0) + 1));
+        return answerCount.keySet().size();
+    }
+
+    private static Stream<String> formatOneGroupPerLine(Stream<String> inputs){
+        List<String> lines = new ArrayList<>();
+        StringBuilder theOneLine = new StringBuilder();
+
+        for(String line : inputs.collect(Collectors.toList())) {
+            if(!line.equals(""))
+                theOneLine.append(line);
+            else{
+                lines.add(theOneLine.toString());
+                theOneLine.setLength(0); // clearing the string builder without using the new keyword.
+            }
+        }
+        lines.add(theOneLine.toString()); // catch the last line.
+        return lines.stream();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(
+                formatOneGroupPerLine(new InputReader().readAsStream("6.txt"))
+                .mapToInt(Day6_1::calcCountOfUniqueAnswersInGroup).sum()
+        );
+    }
+}
---
+++ src/days/Day6_2.java
@@ -0,0 +1,44 @@
+package days;
+
+import utils.InputReader;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day6_2 {
+    private static int calcCountOfUniqueAnswersInGroup(Stream<String> group){
+        List<String> lines = group.collect(Collectors.toList());
+        List<Character> chars = lines.get(0).chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        List<Character> candidateChars = lines.get(0).chars().mapToObj(c -> (char) c).collect(Collectors.toList());
+        for(Character c : chars) {
+            for (String line : lines) {
+                if(!line.contains(c+""))
+                    candidateChars.remove(c);
+            }
+        }
+        return candidateChars.size();
+    }
+
+    private static List<Stream<String>> convertNewLineSeparatedInputToStreamList(Stream<String> inputs){
+        List<Stream<String>> output = new ArrayList<>();
+
+        List<String> tempList = new ArrayList<>();
+        for(String line : inputs.collect(Collectors.toList())){
+            if(!line.equals(""))
+                tempList.add(line);
+            else{
+                output.add(tempList.stream());
+                tempList = new ArrayList<>();
+            }
+        }
+        return output;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println(convertNewLineSeparatedInputToStreamList(new InputReader().readAsStream("6.txt")).stream()
+                .mapToInt(Day6_2::calcCountOfUniqueAnswersInGroup).sum());
+//        System.out.println(calcCountOfUniqueAnswersInGroup(List.of("abc", "cba", "ca", "a").stream()));
+    }
+}
